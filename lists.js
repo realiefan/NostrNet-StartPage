b986ab3b6c4bf41e2bb4ddee0a9f28a4aef89bdf
Iefan <66840141+realiefan@users.noreply.github.com>
2023-11-21 13:17:00 +0000

    main

--- lists.js
+++ lists.js
@@ -1,11 +1,11 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
   // Load links from localStorage
   loadLinks();
 
-  // Move the "Add Link" button to the end of the linksContainer
+  // Attach event listener for toggleDeleteButtons
   document
-    .getElementById("linksContainer")
-    .appendChild(document.getElementById("addLinkButton"));
+    .getElementById("toggleDeleteButton")
+    .addEventListener("click", toggleDeleteButtons);
 });
 
 function openLinkAddingDialog() {
@@ -17,52 +17,18 @@ function closeLinkAddingDialog() {
 }
 
 function addLink() {
-  var title = document.getElementById("newLinkTitle").value;
-  var url = document.getElementById("newLinkURL").value;
+  const title = document.getElementById("newLinkTitle").value;
+  const url = document.getElementById("newLinkURL").value;
 
   if (title && url) {
-    var linkContainer = document.getElementById("linksContainer");
-
-    // Create a new link container
-    var linkDiv = document.createElement("div");
-    linkDiv.className = "link-container";
-
-    // Create a new link button element
-    var linkButton = document.createElement("button");
-    linkButton.textContent = title;
-
-    // Create a delete button element
-    var deleteButton = document.createElement("button");
-    deleteButton.textContent = "Delete";
-    deleteButton.className = "delete-button";
-
-    // Save link data to localStorage
-    var links = JSON.parse(localStorage.getItem("links")) || [];
-    links.push({ title: title, url: url });
+    const links = JSON.parse(localStorage.getItem("links")) || [];
+    links.push({ title, url });
     localStorage.setItem("links", JSON.stringify(links));
 
-    // Add click event to open the link in a new tab
-    linkButton.onclick = function () {
-      window.open(url, "_blank");
-    };
+    const linkContainer = document.getElementById("linksContainer");
 
-    // Add click event to delete the link
-    deleteButton.onclick = function () {
-      // Remove link from localStorage
-      links = links.filter(
-        (link) => !(link.title === title && link.url === url)
-      );
-      localStorage.setItem("links", JSON.stringify(links));
+    const linkDiv = createLinkContainer({ title, url });
 
-      // Remove link container from the DOM
-      linkDiv.remove();
-    };
-
-    // Append link elements to the link container
-    linkDiv.appendChild(linkButton);
-    linkDiv.appendChild(deleteButton);
-
-    // Append the link container to the container
     linkContainer.appendChild(linkDiv);
 
     // Clear input fields
@@ -76,46 +42,55 @@ function addLink() {
   }
 }
 
+function createLinkContainer(link) {
+  const linkDiv = document.createElement("div");
+  linkDiv.className = "link-container";
+
+  const linkButton = document.createElement("button");
+  linkButton.textContent = link.title;
+
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "X";
+  deleteButton.className = "delete-button"; // Initially hidden
+
+  const links = JSON.parse(localStorage.getItem("links")) || [];
+
+  linkButton.addEventListener("click", () => {
+    window.location.href = link.url;
+  });
+
+  deleteButton.addEventListener("click", () => {
+    const updatedLinks = links.filter(
+      (l) => !(l.title === link.title && l.url === link.url)
+    );
+    localStorage.setItem("links", JSON.stringify(updatedLinks));
+    linkDiv.remove();
+  });
+
+  linkDiv.appendChild(linkButton);
+  linkDiv.appendChild(deleteButton);
+
+  return linkDiv;
+}
+
 function loadLinks() {
-  var linkContainer = document.getElementById("linksContainer");
-  var links = JSON.parse(localStorage.getItem("links")) || [];
+  const linkContainer = document.getElementById("linksContainer");
+  const links = JSON.parse(localStorage.getItem("links")) || [];
 
-  links.forEach(function (link) {
-    // Create a new link container
-    var linkDiv = document.createElement("div");
-    linkDiv.className = "link-container";
-
-    // Create a new link button element
-    var linkButton = document.createElement("button");
-    linkButton.textContent = link.title;
-
-    // Create a delete button element
-    var deleteButton = document.createElement("button");
-    deleteButton.textContent = "Delete";
-    deleteButton.className = "delete-button";
-
-    // Add click event to open the link in a new tab
-    linkButton.onclick = function () {
-      window.open(link.url, "_blank");
-    };
-
-    // Add click event to delete the link
-    deleteButton.onclick = function () {
-      // Remove link from localStorage
-      links = links.filter(
-        (l) => !(l.title === link.title && l.url === link.url)
-      );
-      localStorage.setItem("links", JSON.stringify(links));
-
-      // Remove link container from the DOM
-      linkDiv.remove();
-    };
-
-    // Append link elements to the link container
-    linkDiv.appendChild(linkButton);
-    linkDiv.appendChild(deleteButton);
-
-    // Append the link container to the container
+  links.forEach((link) => {
+    const linkDiv = createLinkContainer(link);
     linkContainer.appendChild(linkDiv);
   });
+}
+
+function toggleDeleteButtons() {
+  const deleteButtons = document.querySelectorAll(".delete-button");
+  deleteButtons.forEach((button) => {
+    button.classList.toggle("visible-button");
+  });
+}
+
+function goHome() {
+  const homeLink = "/index.html";
+  window.location.href = homeLink;
 }
